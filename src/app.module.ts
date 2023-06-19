2a54da996db5f0ae43d57a2af268f1d71b8d48e8
Atla33 <dantasanderson57@hotmail.com>
2023-06-19 19:03:38 -0300

    Feita a criação do usuário e a criação do banco com o prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsuarioruModule } from './usuarioru/usuarioru.module';
 //decorator - Javascript - Ecmascript 7
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule, UsuarioruModule],
   controllers: [AppController],
   providers: [AppService],
 })
